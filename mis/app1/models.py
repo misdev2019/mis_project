c11ed941325042bce02627e4966ec4d7bdb7878b
madw <1981409833@qq.com>
2019-05-11 22:11:00 +0800

    更改了一下申请单的模型，并加了个时间表

--- mis/app1/models.py
+++ mis/app1/models.py
@@ -3,6 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+#时间表
+class timetable(models.Model):
+	section = models.IntegerField(primary_key=True,null=False)	#对应每天每一节
+	start_time = models.TimeField(null=False)					#该节开始时间
+	end_time = models.TimeField(null=False)						#该节结束时间
+
+	class Meta:
+		db_table = "timetable"
+		ordering = ['section']
+
 #课室表
 class classroom_equipment_status(models.Model):
 	classroom_id = models.CharField(max_length=20,primary_key=True,null=False) #教室编号
@@ -12,6 +22,7 @@ class classroom_equipment_status(models.Model):
 	class Meta:
 		db_table = "classroom_equipment_status"
 
+#用户信息表
 class userinfo(models.Model):
 	user = models.OneToOneField(User,on_delete=models.CASCADE) 		#用户
 	user_type = models.IntegerField(null=False)						#身份信息
@@ -19,11 +30,13 @@ class userinfo(models.Model):
 	class Meta:
 		db_table = "userinfo"
 
+#申请单表
 class classroom_apply(models.Model):
 	classroom_apply_id = models.CharField(max_length=10,primary_key=True,null=False)	#申请单编号
 	applicant_id = models.CharField(max_length=10,null=False)		#申请人编号
 	classroom_id = models.CharField(max_length=20,null=True)		#教室编号
-	apply_date = models.DateTimeField(null=False)					#教室需要的日期时间
+	apply_date = models.DateField(null=False)						#需求日期
+	apply_section = models.IntegerField(null=False)					#需求节数
 	apply_reason = models.TextField(null=False)						#申请事由
 	apply_type = models.IntegerField(null=False)					#申请类别
 	edu_admin_id = models.CharField(max_length=10,null=True)		#第一审批人编号
@@ -35,10 +48,12 @@ class classroom_apply(models.Model):
 		db_table = "classroom_apply"
 		ordering = ['apply_time']
 
+#课室使用记录表
 class classroom_use_record(models.Model):
 	classroom_id = models.CharField(max_length=20,null=False)		#教室编号
-	use_time = models.DateTimeField(null=False)						#使用时间
+	use_date = models.DateField(null=False)							#使用时间
+	use_section = models.IntegerField(null=False)					#使用节数
 
 	class Meta:
 		db_table = "classroom_use_record"
-		unique_together = ("classroom_id","use_time",)
+		unique_together = ("classroom_id","use_date","use_section")
